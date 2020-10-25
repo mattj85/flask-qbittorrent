833677d6b7902dc4bdfbed84048b2eff4ea5a577
Matt Jones <matt.jones.85@gmail.com>
2020-10-25 17:10:50 +0000

    -

--- api.py
+++ api.py
@@ -5,10 +5,8 @@ from flask_cors import CORS
 from src.config import config
 from src.qbittorrent import torrents
 
-torrentApi = Flask(__name__)
-CORS(torrentApi)
-
-torrentApi.register_blueprint(torrents)
+api = Flask(__name__)
+api.register_blueprint(torrents)
 
 if __name__ == "__main__":
-    torrentApi.run(host=config.APPHOST, port=config.APPPORT)
+    api.run(host=config.APPHOST, port=config.APPPORT)
